4280c8121f9941726ebb20fc51447f30e2cbb47a
Fabio.Classo-ext <Fabio.Classo-ext@ab-Inbev.com>
2025-09-09 12:32:51 -0300

    Adding run sh

---
+++ tests/run.sh
@@ -0,0 +1,10 @@
+# executin all python classes
+
+
+python ecommerce_analysis.py
+python demo_ecommerce_analyzer.py
+python generate_fake_data.py
+python generate_bad_records.py
+python quality_check_schema.py
+python quality_check_data.py
+python test_generate_metrics_dataframes.py
